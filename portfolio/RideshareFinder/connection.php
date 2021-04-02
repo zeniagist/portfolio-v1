653f1dafa948adf38e751ab498c29eab52063a15
Zenia Gist <53485908+zeniagist@users.noreply.github.com>
2021-04-01 21:14:49 -0500

    Delete portfolio/RideshareFinder directory
    need to update

--- portfolio/RideshareFinder/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // <!--Connect to the database-->
-    $link = mysqli_connect("localhost", "zeniagi1_rideshare", "Eiffeltower10", "zeniagi1_rideshare");
-
-    if(mysqli_connect_error()){
-        die("ERROR: Unable to connect: " . mysqli_connect_error());
-    }
-?>
