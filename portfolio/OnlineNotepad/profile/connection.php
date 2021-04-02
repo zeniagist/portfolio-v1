53a208a7389e25b0702d1fa8375db1ce7707112b
Zenia Gist <53485908+zeniagist@users.noreply.github.com>
2021-04-01 21:29:49 -0500

    Delete portfolio/OnlineNotepad directory
    php not supported on github pages, adding to .gitignore

--- portfolio/OnlineNotepad/profile/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // <!--Connect to the database-->
-    $link = mysqli_connect("localhost", "zeniagi1_onlinenotes", "Eiffeltower10", "zeniagi1_onlinenotes");
-
-    if(mysqli_connect_error()){
-        die("ERROR: Unable to connect: " . mysqli_connect_error());
-    }
-?>
